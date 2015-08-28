c82cfdf4790dea6aa964c6f7fe6a69f52af0a005
Dan Brakeley <dbrakeley@proletariat.com>
2015-08-28 09:59:21 -0400

    actually use the new Ok struct

--- src/amqplib/AMQPLib.hx
+++ src/amqplib/AMQPLib.hx
@@ -18,22 +18,22 @@ typedef Message = {
 
 typedef Channel = {
   function close(cb:Err->Void):Void;
-  function assertQueue(queue:String, options:Dynamic, cb:Err->Dynamic->Void):Void;
-  function checkQueue(queue:String, cb:Err->Dynamic->Void):Void;
-  function deleteQueue(queue:String, options:Dynamic, cb:Err->Dynamic->Void):Void;
-  function purgeQueue(queue:String, cb:Err->Dynamic->Void):Void;
-  function bindQueue(queue:String, source:String, pattern:String, args:Dynamic, cb:Err->Dynamic->Void):Void;
-  function unbindQueue(queue:String, source:String, pattern:String, cb:Err->Dynamic->Void):Void;
-  function assertExchange(exchange:String, type:String, options:Dynamic, cb:Err->Dynamic->Void):Void;
-  function checkExchange(exchange:String, cb:Err->Dynamic->Void):Void;
-  function deleteExchange(exchange:String, options:Dynamic, cb:Err->Dynamic->Void):Void;
-  function bindExchange(destination:String, source:String, pattern:String, args:Dynamic, cb:Err->Dynamic->Void):Void;
-  function unbindExchange(destination:String, source:String, pattern:String, args:Dynamic, cb:Err->Dynamic->Void):Void;
+  function assertQueue(queue:String, options:Dynamic, cb:Err->Ok->Void):Void;
+  function checkQueue(queue:String, cb:Err->Ok->Void):Void;
+  function deleteQueue(queue:String, options:Dynamic, cb:Err->Ok->Void):Void;
+  function purgeQueue(queue:String, cb:Err->Ok->Void):Void;
+  function bindQueue(queue:String, source:String, pattern:String, args:Dynamic, cb:Err->Ok->Void):Void;
+  function unbindQueue(queue:String, source:String, pattern:String, cb:Err->Ok->Void):Void;
+  function assertExchange(exchange:String, type:String, options:Dynamic, cb:Err->Ok->Void):Void;
+  function checkExchange(exchange:String, cb:Err->Ok->Void):Void;
+  function deleteExchange(exchange:String, options:Dynamic, cb:Err->Ok->Void):Void;
+  function bindExchange(destination:String, source:String, pattern:String, args:Dynamic, cb:Err->Ok->Void):Void;
+  function unbindExchange(destination:String, source:String, pattern:String, args:Dynamic, cb:Err->Ok->Void):Void;
   function publish(exchange:String, routingKey:String, content:NodeBuffer, options:Dynamic):Void;
   @:overload(function(queue:String, content:NodeBuffer):Void {})
   function sendToQueue(queue:String, content:NodeBuffer, options:Dynamic):Void;
-  function consume(queue:String, callback:Message->Void, options:Dynamic, cb:Err->Dynamic->Void):Void;
-  function cancel(consumerTag:String, cb:Err->Dynamic->Void):Void;
+  function consume(queue:String, callback:Message->Void, options:Dynamic, cb:Err->Ok->Void):Void;
+  function cancel(consumerTag:String, cb:Err->Ok->Void):Void;
   function get(queue:String, options:Dynamic, cb:Err->Dynamic->Void):Void;
   function ack(message:Message, allUpTo:Bool):Void;
   function ackAll():Void;
@@ -42,7 +42,7 @@ typedef Channel = {
   function reject(message:Message, requeue:Bool):Void;
   @:overload(function (count:Int):Void {})
   function prefetch(count:Int, global:Dynamic, cb:Err->Dynamic->Void):Void;
-  function recover(cb:Err->Dynamic->Void):Void;
+  function recover(cb:Err->Ok->Void):Void;
 }
 
 typedef Connection = {
